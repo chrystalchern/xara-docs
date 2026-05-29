b189cd8ba1806cb24972acfd824f8e3e5e32f0a7
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-05-29 13:45:14 -0700

    Create prolog.py

---
+++ source/_setup/prolog.py
@@ -0,0 +1,64 @@
+
+rst_prolog = """
+.. |floatList| replace:: *list float*
+.. |integerList| replace:: *list integer*
+.. |integerTuple| replace:: *tuple integer*
+.. |intList| replace:: *list integer*
+.. |listFloat| replace:: *list float*
+.. |listInteger| replace:: *list integer*
+.. |listInt| replace:: *list integer*
+.. |floatTuple| replace:: *tuple of float*
+.. |list| replace:: *list*
+.. |kwds| replace:: `kwds <kwds>`__
+.. |bool| replace:: *bool*
+.. |string| replace:: *string*
+.. |str|  replace:: *str*
+.. |float| replace:: *float*
+.. |integer| replace:: *integer*
+.. |OPS| replace:: xara
+.. |BRACE2| replace:: `BRACE2`_
+.. _BRACE2: https://structures.live
+.. only:: html
+
+    .. |xara| raw:: html
+
+       <span style="font-family: 'Inconsolata', sans-serif;">𝜒ara</span>
+
+.. |OpenSeesRT| replace:: `OpenSeesRT`_
+.. _OpenSeesRT: https://stairlab.berkeley.edu/software/opensees/
+.. |OpenSees| replace:: OpenSees
+.. |OPS link| replace:: `OpenSees app`_
+.. _OpenSees app: https://stairlab.berkeley.edu/software/OpenSeesRT/
+.. |githubLink| replace:: `OpenSees Github link`_
+.. _OpenSees Github link: https://github.com/peer-open-source/xara
+.. |messageBoard| replace:: `OpenSees Message Board`_
+.. _OpenSees Message Board: https://github.com/claudioperez/OpenSeesRT/discussions
+.. |glf| replace:: `Gregory L. Fenves`_
+.. _Gregory L. Fenves: http://www.caee.utexas.edu/faculty/directory/fenves
+.. only:: html
+
+   .. |mhs| raw:: html
+
+      <a href="https://web.engr.oregonstate.edu/~mhscott/" rel="nofollow">Michael H. Scott</a>
+..
+    .. only:: not html
+
+       .. |mhs| replace:: `Michael H. Scott`_
+       .. _Michael H. Scott: https://cce.oregonstate.edu/scott
+
+.. |fmk| replace:: *fmk*
+.. |fcf| replace:: `fcf`_
+.. _fcf: https://fedeas.net
+.. |rms| replace:: *Remo Magalhaes de Souza*
+.. |cmp| replace:: `Claudio M. Perez`_
+.. _Claudio M. Perez: https://stairlab.berkeley.edu/people/claudioperez
+.. |pedro| replace:: `Pedro Arduino`_
+.. _Pedro Arduino: https://www.ce.washington.edu/facultyfinder/pedro-arduino
+.. |peter| replace:: `Peter Mackenzie-Helnwein`_
+.. _Peter Mackenzie-Helnwein: https://www.ce.washington.edu/facultyfinder/peter-mackenzie-helnwein
+.. |chris| replace:: `Chris McGann`_
+.. _Chris McGann: https://www.canterbury.ac.nz/engineering/contact-us/people/chris-mcgann.html
+.. |andreas| replace:: *Andreas Schellenberg*
+.. |csasj| replace:: *csasj*
+
+"""
